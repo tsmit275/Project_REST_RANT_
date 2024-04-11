3d193ecc3cf3edc8cfcf8b170f428012886a7ea1
tsmit275 <taylor.lsmith03@gmail.com>
2024-04-11 19:14:20 -0400

    Adding remaining stub routes

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const express = require('express');
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+    res.send('Hello world!')
+})
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+app.listen(process.env.PORT)
