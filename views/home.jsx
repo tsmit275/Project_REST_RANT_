788afa609146258a232691c1200c09e1a03603dc
tsmit275 <taylor.lsmith03@gmail.com>
2024-04-15 22:00:34 -0400

    Adding styles to to REST-Rant

--- views/home.jsx
+++ views/home.jsx
@@ -1,15 +1,17 @@
-const React = require('react')
-const Def = require('./default')
+const React = require("react");
+const Def = require("./default");
 
-function home () {
-    return (
-        <Def>
-            <main>
-                <h1>HOME</h1>
-            </main>
-        </Def>
-    )
-
+function home() {
+  return (
+    <Def>
+      <main>
+        <h1>HOME</h1>
+        <a href="/places">
+          <button className="btn-primary">Places Page</button>
+        </a>
+      </main>
+    </Def>
+  );
 }
 
-module.exports = home 
+module.exports = home;
