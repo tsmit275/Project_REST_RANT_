09e617160170d7115187580ef0b47dbf37d978ca
tsmit275 <taylor.lsmith03@gmail.com>
2024-04-18 16:09:04 -0400

    Adding and rendering a new view

--- controllers/places.js
+++ controllers/places.js
@@ -1,13 +1,17 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 router.get('/', (req, res) => {
     let places = [
         { name: 'Restaurant A', city: 'City A', state: 'State A', cuisines: 'Cuisines A', pic: 'https://images.pexels.com/photos/128756/pexels-photo-128756.jpeg?auto=compress&cs=tinysrgb&w=600' },
         { name: 'Restaurant B', city: 'City B', state: 'State B', cuisines: 'Cuisines B', pic: 'https://images.pexels.com/photos/406014/pexels-photo-406014.jpeg?auto=compress&cs=tinysrgb&w=600' }
-    ];
+    ]
 
     res.render('places/index', { places: places }); // Pass places array to the view
-});
+})
 
+router.get('/new', (req, res) => {
+    res.render('places/new')
+  })
+  
 module.exports = router;
