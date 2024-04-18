7a46282fa2f91e75046f23b9847b643b0dd8af45
tsmit275 <taylor.lsmith03@gmail.com>
2024-04-18 18:09:20 -0400

    Jazzed up form page

--- controllers/places.js
+++ controllers/places.js
@@ -13,5 +13,13 @@ router.get('/', (req, res) => {
 router.get('/new', (req, res) => {
     res.render('places/new')
   })
+
+  router.post('/', (req, res) => {
+    console.log(req.body)
+    res.send('POST /places')
+  })
   
-module.exports = router;
+
+
+  
+module.exports = router
