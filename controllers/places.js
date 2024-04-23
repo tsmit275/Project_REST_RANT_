dba0c2942defacf9f03d57f9a1c7791e35bb5d72
tsmit275 <taylor.lsmith03@gmail.com>
2024-04-23 06:25:33 -0400

    Added edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -1,67 +1,89 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const places = require('../models/places.js');
+const places = require("../models/places.js");
 
 router.use(express.urlencoded({ extended: true }));
 
-router.get('/', (req, res) => {
-    res.render('places/index', { places: places }) ;
+router.get("/", (req, res) => {
+  res.render("places/index", { places: places });
 });
 
-router.get('/new', (req, res) => {
-    res.render('places/new');
+router.get("/new", (req, res) => {
+  res.render("places/new");
 });
 
-router.post('/', (req, res) => {
-    if (!req.body.pic) {
-      // Default image if one is not provided
-      req.body.pic = '/images/saddog.png';
-    }
-    if (!req.body.city) {
-      req.body.city = 'Anytown';
-    }
-    if (!req.body.state) {
-      req.body.state = 'USA';
-    }
-    places.push(req.body);
-    res.redirect('/places');
+router.post("/", (req, res) => {
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = "/images/saddog.png";
+  }
+  if (!req.body.city) {
+    req.body.city = "Anytown";
+  }
+  if (!req.body.state) {
+    req.body.state = "USA";
+  }
+  places.push(req.body);
+  res.redirect("/places");
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id) || id < 0 || id >= places.length) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    res.render("places/show", { place: places[id], id: id });
+  }
+});
+
+router.delete("/:id", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    places.splice(id, 1);
+    res.redirect("/places");
+  }
+});
+
+router.get("/:id/edit", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id) || id < 0 || id >= places.length) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    res.render("places/edit", { place: places[id], id });
+  }
+});
+
+router.put('/:id', (req, res) => {
     let id = Number(req.params.id);
-    if (isNaN(id) || id < 0 || id >= places.length){
+    if (isNaN(id) || id < 0 || id >= places.length) {
         res.render('error404');
     } else if (!places[id]) {
         res.render('error404');
     } else {
-        res.render('places/show', { place: places[id], id: id});
-    }
-});
-
-router.delete('/:id', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-      res.render('error404')
-    }
-    else if (!places[id]) {
-      res.render('error404')
-    }
-    else {
-      places.splice(id, 1)
-      res.redirect('/places')
-    }
-  })
+        // Dig into req.body and make sure data is valid
+        if (!req.body.pic) {
+            // Default image if one is not provided
+            req.body.pic = 'http://placekitten.com/400/400';
+        }
+        if (!req.body.city) {
+            req.body.city = 'Anytown';
+        }
+        if (!req.body.state) {
+            req.body.state = 'USA';
+        }
   
-  router.get('/:id/edit', (req, res) => {
-    let id = Number(req.params.id);
-    if (isNaN(id) || id < 0 || id >= places.length){
-        res.render('error404');
-    } else if (!places[id]) {
-        res.render('error404');
-    } else {
-        res.render('places/edit', { place: places[id], id });
+        // Save the new data into places[id]
+        places[id] = req.body;
+        res.redirect(`/places/${id}`);
     }
 });
-
 
 module.exports = router;
