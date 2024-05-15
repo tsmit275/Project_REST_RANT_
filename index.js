d8d54a60e4b96a67badb700b3d0d05086ded7091
tsmit275 <taylor.lsmith03@gmail.com>
2024-05-14 22:54:13 -0400

    made som changes

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
-//Modules and Globals
-require("dotenv").config()
-const mongoose = require('mongoose')
-const express = require("express")
-const app = express()
+// Modules and Globals
+require('dotenv').config()
+const express = require('express')
 const methodOverride = require('method-override')
+const app = express()
+const mongoose = require('mongoose')
 
-//Express Settings
+
+// Express Settings
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true}))
 app.use(methodOverride('_method'))
+const MONGO_URI = process.env.MONGO_URI
+const PORT = process.env.PORT
 
-//Controllers and Routes
-app.use("/places", require("./controllers/places"))
+// Controllers & Routes
+app.use('/places', require('./Controllers/places'))
 
-app.get("/", (req, res)=>{
-    res.render("home")
+
+app.get('/', (req, res) => {
+    res.render('home')
 })
 
-app.get("*", (req, res) => {
+app.get('*', (req, res) => {
     res.render('error404')
 })
 
-const PORT = process.env.PORT
-
-app.listen(PORT, console.log("listening on port ",PORT))
-
-
-console.log("load index.js")
-
+// Listen for Connections
+const start = async () => {
+    await mongoose.connect(MONGO_URI);
+    console.log('Connected to database')
+    app.listen(PORT, () => {
+      console.log('listening on port', PORT);
+    })
+  }
+  
+  start()
